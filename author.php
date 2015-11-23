7d980116909249676e69483503168db34bfcfbb0
Caleb Evans <caleb@calebevans.me>
2015-11-22 16:12:36 -0800

    Remove unnecessary condition in author template

--- author.php
+++ author.php
@@ -7,22 +7,14 @@
 
 get_header(); ?>
 
-	<?php if ( is_author() ): ?>
+	<?php $author_name = get_the_author(); ?>
+	<?php if ( ! empty( $author_name ) && 0 !== get_the_author_posts() ): ?>
 
-		<?php $author_name = get_the_author(); ?>
-		<?php if ( ! empty( $author_name ) && 0 !== get_the_author_posts() ): ?>
+		<p>These are all of the blog posts by the user <strong><?php echo $author_name; ?></strong>.</p>
 
-			<p>These are all of the blog posts by the user <strong><?php echo $author_name; ?></strong>.</p>
+	<?php else: ?>
 
-		<?php else: ?>
-
-			<p>There are no posts for the user <strong><?php echo $author_name; ?></strong>.</p>
-
-		<?php endif; ?>
-
-	<?php elseif ( is_home() && ! is_front_page() ): ?>
-
-		<?php echo apply_filters( 'the_content', get_queried_object()->post_content ); ?>
+		<p>There are no posts for the user <strong><?php echo $author_name; ?></strong>.</p>
 
 	<?php endif; ?>
 
