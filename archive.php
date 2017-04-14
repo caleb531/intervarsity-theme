3e03e34b8e2cf0b3d0c67dee04900416112490df
Caleb Evans <caleb@calebevans.me>
2017-04-13 20:48:45 -0700

    Allow the content for a post type to be overridden

--- archive.php
+++ archive.php
@@ -28,7 +28,7 @@ get_header(); ?>
 
 				<?php while ( have_posts() ) : the_post(); ?>
 
-					<?php get_template_part( 'content' ); ?>
+					<?php get_template_part( 'content', get_post_type() ); ?>
 
 				<?php endwhile; wp_reset_query(); ?>
 
